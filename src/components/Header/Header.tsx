69604d8cbd0372603a3b67606448607aeb192759
Letam Bossman Barinua <letambarinua@gmail.com>
2022-12-18 12:49:14 +0100

    Add padding and background colour to header element

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,12 @@ import styled from "styled-components";
 const StyledHeader = styled.header`
   padding: 5px;
   display: flex;
+  padding: 20px 25px;
   align-items: center;
+  border-bottom: 2px solid;
+  background-color: #292123;
   justify-content: space-between;
-
+  
   button {
     border: none;
     height: 30px;
