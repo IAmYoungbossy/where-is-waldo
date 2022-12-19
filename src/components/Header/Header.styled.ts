79ac9d50e1835990daa71bf6f291626853b68732
Letam Bossman Barinua <letambarinua@gmail.com>
2022-12-19 11:15:52 +0100

    Create Header.styled.ts for styling of header element

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  padding: 5px;
+  display: flex;
+  padding: 20px 25px;
+  align-items: center;
+  border-bottom: 2px solid;
+  background-color: #292123;
+  justify-content: space-between;
+  
+  button {
+    border: none;
+    height: 30px;
+    color: white;
+    padding: 0px 15px;
+    border-radius: 5px;
+    background-color: #6c757d;
+  }
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,4 @@
-import styled from "styled-components";
-
-const StyledHeader = styled.header`
-  padding: 5px;
-  display: flex;
-  padding: 20px 25px;
-  align-items: center;
-  border-bottom: 2px solid;
-  background-color: #292123;
-  justify-content: space-between;
-  
-  button {
-    border: none;
-    height: 30px;
-    color: white;
-    padding: 0px 15px;
-    border-radius: 5px;
-    background-color: #6c757d;
-  }
-`;
+import { StyledHeader } from "./Header.styled";
 
 export default function Header() {
   return (
