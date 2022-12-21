15cdc64f6f964e3c87c57e1b0676555ffcee8b7f
Letam Bossman Barinua <letambarinua@gmail.com>
2022-12-21 01:49:54 +0100

    Change Header from where's waldo to Hidden folksy

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import { StyledHeader } from "./Header.styled";
 
-export default function Header():JSX.Element {
+export default function Header(): JSX.Element {
   return (
     <StyledHeader>
-      <h1>Where's Waldo</h1>
+      <h1>Hidden Folks</h1>
       <button>Log Out</button>
     </StyledHeader>
   );
